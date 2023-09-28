186f5077315ebbeb837e917cb82ad7997059e35c
GTR1701 <dawidkowal04@gmail.com>
2023-09-28 10:23:52 +0200

    testt

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -11,10 +11,10 @@
 		</svg>
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
+				<a href="/root">Home</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+				<a href="/root/about">About</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
